7d0c5a52e932bc8e72d2f29e9183924d29197b67
Hartecky <hofman.bartlomiej@gmail.com>
2021-07-05 15:06:02 +0200

    update

--- plots/fluorescence_plot.R
+++ plots/fluorescence_plot.R
@@ -5,6 +5,7 @@ fluorescence.plot <- function(melt.data){
   
   hrm.plot <- ggplot(melt.data, aes(Temperature, FluorescenceSignal, col=Sample)) + 
     geom_line(size = 0.75) +
+    ggtitle("High Resolution Melt visualisation plot") +
     xlab("Temperature") +
     ylab("Fluorescence Signal") +
     ggtitle("HRM Melting Curves")
