86a3e8561acb63c3d9995e1953b66102fe8e78ed
Hartecky <hofman.bartlomiej@gmail.com>
2021-07-07 13:44:04 +0200

    base plots functions

---
+++ plots/base_plot.R
@@ -0,0 +1,42 @@
+library(ggplot2)
+library(plotly)
+
+hist.plot <- function(x){
+  gg.hist <- ggplot() +
+    geom_histogram(aes(x), bins = 25) +
+    ggtitle("Histogram plot") +
+    xlab("Distribution series") +
+    ylab("Count")
+  
+  ggplotly(gg.hist)
+}
+
+box.plot <- function(x, var){
+  gg.box <- ggplot() +
+    geom_boxplot(aes(y = x)) +
+    ggtitle("Boxplot") +
+    xlab(paste0("Variable: ", var)) +
+    ylab("Counts")
+  
+  ggplotly(gg.box)
+}
+
+scatter.plot <- function(x, var){
+  gg.scatter <- qplot(x = seq_along(x),
+                      y = x, 
+                      xlab = paste0("Variable ", var, " index"),
+                      ylab = "Value", 
+                      main = "Scatter plot")
+
+  ggplotly(gg.scatter)
+}
+
+generate.plot <- function(x, var, plot.option){
+  if (plot.option == 'histogram') {
+    hist.plot(x)
+  } else if (plot.option == 'boxplt') {
+    box.plot(x, var)
+  } else if (plot.option == 'scatter') {
+    scatter.plot(x, var)
+  }
+}
