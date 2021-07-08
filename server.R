97efd77db27304da5e6a36e188e39ccce7488c4a
Hartecky <hofman.bartlomiej@gmail.com>
2021-07-08 09:48:00 +0200

    pinned up lod laculations, lod plots, fluorescence plots

--- server.R
+++ server.R
@@ -23,6 +23,7 @@ server <- function(input, output, session) {
         )
     })
     
+    # RENDER DATATABLE AND SUMMARY STATISTICS FOR GIVEN DATA -------------------
     output$contents <- renderDataTable({
         req(input$file1)
         
@@ -72,6 +73,8 @@ server <- function(input, output, session) {
     
     # PLOTS --------------------------------------------------------------------
     output$base_plots_output <- renderPlotly({
+        
+        # Require selected input and specified plot type
         req(input$select.variable, input$plot.type)
         
         X <- data()[[input$select.variable]]
@@ -84,9 +87,134 @@ server <- function(input, output, session) {
                             "Number of bins:",
                             min = 1,
                             max = 50,
-                            value = 30)
+                            value = 30, 
+                            width = '100%')
+                
             })
+        } else if (option != 'histogram'){
+                output$hist.slider <- renderUI({ })
             }
+        
         generate.plot(X, variable, option, input$bins)
         })
+    
+    # LIMIT OF DETECTION CALCULATION -------------------------------------------
+    lod.data <- reactive({
+
+        # Require provided input file with data
+        req(input$file1)
+        lod.matrix <- data()
+
+        # Check the dimension of the provided data
+        if(dim(lod.matrix)[1]==3 & dim(lod.matrix)[2]==3) {
+
+            # Calculate differences between total and positives samples
+            Y = define.freq(lod.matrix)
+
+            # Prepare logistic model
+            model.glm <- fit.model(Y, lod.matrix)
+
+            # Predict values at 95% significance level
+            level = logit.value(0.95)
+
+            X.LOD <- LOD(model.glm, level)
+
+            log.SE.LOD <- se.computations(model.glm,
+                                          X.LOD)
+
+            plot.labs <- prepare.set(model.glm,
+                                     X.LOD,
+                                     level)
+            
+            pred.data <- plot.labs$pred.df
+            top.interval <- plot.labs$top.interval
+            bottom.interval <- plot.labs$bottom.interval
+
+            # Return calculation results into DataFrame
+            lod.data = list(pred.data = pred.data,
+                            top.interval = top.interval,
+                            bottom.interval = bottom.interval,
+                            x.lod = X.LOD,
+                            significance = 0.95,
+                            logit.value = level,
+                            se.lod = log.SE.LOD)
+        } else {
+            # If data is not a 3x3 matrix with specified column
+            # names, then stop running this function and return
+            # warning info
+            stop("Data provided for LOD calculation is incorrect. Exit code with status 1.
+            
+                 Example data for LOD calculation:
+                 
+                 dilution;total;positive
+                 6;8;4
+                 12;8;8
+                 24;8;8")
+        }
+    })
+    
+    # LIMIT OD DETECTION PLOT --------------------------------------------------
+    output$lod_plots_output <- renderPlotly({
+        
+        # Require provided input file with data, and also
+        # require calculated coefficients from LOD calculations
+        req(input$file1, lod.data())
+        
+        # Return Limit of Detection interactive plot with pointed values
+        lod.plot(data = data(),
+                 pred.data = lod.data()$pred.data, 
+                 top.interval = lod.data()$top.interval, 
+                 bottom.interval = lod.data()$bottom.interval, 
+                 X.LOD = lod.data()$x.lod)
+    })
+    
+    # LIMIT OF DETECTION STATISTICS --------------------------------------------
+    output$lod_stats_output <- renderPrint({
+        
+        # Require provided input file with data
+        req(input$file1, lod.data())
+        
+        # Return a dataframe with calculated values
+        data.frame(significance = lod.data()$significance,
+                   logit.value = lod.data()$logit.value,
+                   lod.value = lod.data()$x.lod,
+                   se.lod.value = lod.data()$se.lod)
+    })
+    
+    # FLUORESCENCE DATA PREPARATION --------------------------------------------
+    pcr.data <- reactive({
+        
+        req(input$file1)
+        
+        pcr.tmp <- data()
+        
+        if(colnames(pcr.tmp)[1]=="Temperature"){
+            pcr.melt = melting.data(dataframe = pcr.tmp, id_variables = "Temperature")
+        } else {
+            stop("
+                 Data provided for qPCR fluorescence visualisation is incorrect. Exit code with status 1.
+                 Common problems:
+                 - First column is not the 'Temperature' column;
+                 - incorrect separators for values or digits;
+                 - undeclared header;
+                 - wrong first column name
+                 
+                 Example of the correct data:
+                 
+                 Temperature; 1; 2; 3
+                 79,99; 89,38; 89,39; 89,54
+                 80,00; 89,34; 89,35; 89,50
+                 80,01; 89,30; 89,31; 89,46
+                 80,02; 89,25; 89,27; 89,42
+                 ")
+        }
+    })
+    
+    output$qpcr_plot_output <- renderPlotly({
+        # Require provided input file with data
+        req(input$file1)
+        
+        # Return overview of the data with head
+        fluorescence.plot(pcr.data())
+    })
 }
