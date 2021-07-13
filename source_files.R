0b0e4ebbc900c8f194c5e72f2aba7381d62627c4
Hartecky <hofman.bartlomiej@gmail.com>
2021-07-13 10:54:47 +0200

    Delete requirements and source files

--- source_files.R
+++
@@ -1,9 +0,0 @@
-sourceFolder <- function(folder, recursive = FALSE, ...) 
-{ 
-  files <- list.files(folder, pattern = "[.][rR]$", 
-                      full.names = TRUE, recursive = recursive)
-  if (!length(files))
-    stop(simpleError(sprintf('No R files in folder "%s"', folder)))
-  src <- invisible(lapply(files, source, ...))
-  message(sprintf('%s files sourced from folder "%s"', length(src), folder))
-}
