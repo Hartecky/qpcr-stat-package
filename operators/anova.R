0412926d70d253b42e8bde0fd65f35fe6d64f055
Hartecky <hofman.bartlomiej@gmail.com>
2021-07-14 10:44:56 +0200

    anova parametric / non-parametric functions

---
+++ operators/anova.R
@@ -0,0 +1,42 @@
+analysis.of.variance <- function(Y, X, data) {
+
+  fit <- aov(Y ~ X, data = data)
+  fitted <- anova(fit)
+  return(list(fitted.model = fitted,
+              model = fit))
+}
+
+analysis.of.variance.two <- function(Y, X1, X2, data){
+  fit <- aov(Y ~ X1 + X2, data = data)
+  fitted <- anova(fit)
+  return(list(fitted.model = fitted,
+              model = fit))
+}
+
+analysis.posthoc <- function(model, fitted.model) {
+  if (fitted.model$`Pr(>F)`[1] < 0.05) {
+    TukeyHSD(model)
+  }
+}
+
+kruskal.analysis <- function(Y, X, data){
+  model <- kruskal.test(Y ~ X, data)
+  
+  print(model)
+}
+
+kruskal.posthoc <- function(Y, X, model, data){
+  if (model$p.value < 0.05){
+    DT <- dunnTest(Y ~ X, data = data)
+    
+    print(DT)
+    
+    PT <- DT$res
+    
+    cldList(P.adj ~ Comparison,
+            data = PT,
+            threshold = 0.05)
+  } else {
+    return()
+  }
+}
