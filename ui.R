ff4c2d0e5e43ca2d243156f56e8ac0f46d0929f4
Hartecky <hofman.bartlomiej@gmail.com>
2021-08-16 10:02:20 +0200

    divided ui into separated files

--- ui.R
+++ ui.R
@@ -20,15 +20,16 @@ library(shinydashboard)
 CURRENT.PATH <- dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(CURRENT.PATH)
 
-# HOME LINUX SOURCE FILES ------------------------------------------------------
-# OPERATORS FUNCTIONS
-# source('/home/hartek/AmpliStat/operators/configure_lod_set.R')
-# source('/home/hartek/AmpliStat/operators/preprocessing_lod_data.R')
-# source('/home/hartek/AmpliStat/operators/melt_data.R')
-#
-# PLOTTING FUNCTIONS
-# source('/home/hartek/AmpliStat/plots/generate_lod_plot.R')
-# source('/home/hartek/AmpliStat/plots/fluorescence_plot.R')
+# USER INTERFACE COMPONENTS ----------------------------------------------------
+source('ui/data.upload.R')
+source('ui/data.vis.R')
+source('ui/assumptions.ui.R')
+source('ui/mc.parametric.ui.R')
+source('ui/mc.nonparametric.ui.R')
+source('ui/aov.parametric.ui.R')
+source('ui/aov.nonparametric.ui.R')
+source('ui/lod.ui.R')
+source('ui/hrm.ui.R')
 
 # WORK OFFICE SOURCE FILES -----------------------------------------------------
 source('operators/configure_lod_set.R')
@@ -49,321 +50,46 @@ ui <- fluidPage(
   navbarPage(
     'AmpliStat',
     
-    # DATA UPLOAD UI PANEL -------------------------------------------------------
-    tabPanel(
-      strong('Data Upload'),
-      titlePanel('Uploading Files'),
-      tags$hr(),
-      
-      sidebarLayout(
-        sidebarPanel(
-          fileInput(
-            'file1',
-            'Choose CSV file',
-            multiple = T,
-            accept = c("text/csv",
-                       "text/comma-separated-values,text/plain",
-                       ".csv")
-          ),
-          checkboxInput("header", "Header", TRUE),
-          selectInput(
-            "sep",
-            "Separator",
-            choices = c(
-              Comma = ",",
-              Semicolon = ";",
-              Tab = "\t"
-            ),
-            selected = ","
-          ),
-          selectInput(
-            "dec",
-            "Decimal",
-            choices = c(Dot = '.', Comma = ","),
-            selected = '.'
-          ),
-          tags$hr(),
-          checkboxInput('string', "Strings as factor", TRUE),
-          tags$hr(),
-          
-          selectInput(
-            "disp",
-            "Display",
-            choices = c(Head = "head",
-                        All = "all"),
-            selected = "head"
-          ),
-          submitButton("Update view", icon("refresh"))
-        ),
-        
-        mainPanel(
-          dataTableOutput("contents"),
-          verbatimTextOutput('contents.stats'),
-          width = 8
-        )
-      )
-    ),
+    # DATA UPLOAD UI PANEL -----------------------------------------------------
+    data.upload,
     
-    # DATA VISUALISATION PANEL ---------------------------------------------------
-    tabPanel(
-      'Data Visualisation',
-      titlePanel("Data Visualisation Panel"),
-      tags$hr(),
-      sidebarLayout(
-        sidebarPanel(
-          strong('Select variable'),
-          tags$hr(),
-          uiOutput('select.variable'),
-          tags$hr(),
-          selectInput(
-            'plot.type',
-            'Choose plot type',
-            choices = c(
-              Histogram = 'histogram',
-              Boxplot = 'boxplt',
-              Scatter = 'scatter'
-            )
-          ),
-          submitButton()
-        ),
-        mainPanel(
-          plotlyOutput('base_plots_output'),
-          uiOutput('hist.slider'),
-          width = 8
-        )
-      )
-    ),
+    # DATA VISUALISATION PANEL -------------------------------------------------
+    data.vis,
     
-    # ASSUMPTIONS TESTING PANEL --------------------------------------------------
-    tabPanel(
-      'Assumptions',
-      titlePanel("Check assumptions panel"),
-      tags$hr(),
-      sidebarLayout(
-        sidebarPanel(
-          strong("Select variable"),
-          tags$hr(),
-          uiOutput('select.variable.assum1'),
-          tags$hr(),
-          uiOutput('select.variable.assum2'),
-          tags$hr(),
-          selectInput(
-            'check.assumps',
-            'Choose analysis',
-            choices = c(
-              `Distribution normality` = 'normtest',
-              `Variance Homogenity` = 'vartest',
-              `Outliers Detection` = 'outliers'
-            )
-          ),
-          submitButton()
-        ),
-        mainPanel(
-          verbatimTextOutput('assumptions_messages'),
-          tags$hr(),
-          verbatimTextOutput('assumptions_output'),
-          tags$hr(),
-          verbatimTextOutput('assumptions_interpret')
-        )
-      )
-    ),
+    # ASSUMPTIONS TESTING PANEL ------------------------------------------------
+    assumptions,
     
-    # MEANS COMPARISON MENU ------------------------------------------------------
+    # MEANS COMPARISON MENU ----------------------------------------------------
     navbarMenu(
       'Means Comparison',
       
-      # PARAMETRIC TESTS ------------------------------------------------
-      tabPanel(
-        'Parametric',
-        titlePanel("Parametric tests for comparing means between samples"),
-        sidebarLayout(
-          sidebarPanel(
-            selectInput(
-              't.test.type',
-              'T-test type',
-              choices = c(`One Sample` = "onesample",
-                          `Two Samples` = "twosamples")
-            ),
-            tags$hr(),
-            helpText("Select variable"),
-            uiOutput('select.variable.mcp1'),
-            uiOutput('select.variable.mcp2'),
-            tags$hr(),
-            strong("T-TEST PARAM OPTIONS"),
-            helpText("Choose option below when comparing one sample"),
-            numericInput('mu', 'Mean to compare with', 0),
-            
-            selectInput(
-              'par_ttest',
-              'Paired',
-              choices = c(Paired = 'Paired',
-                          `Non-paired` = 'Non-paired')
-            ),
-            selectInput(
-              'alternative_ttest_p',
-              'Alternative',
-              choices = c(
-                Less = "greater",
-                Greater = "less",
-                `Two Sided` = "two.sided"
-              )
-            ),
-            numericInput(
-              'alpha_testt',
-              'Significance level',
-              value = 0.05,
-              step = 0.01
-            ),
-            submitButton()
-          ),
-          
-          mainPanel(verbatimTextOutput('means_param_output'))
-        )
-      ),
+      # PARAMETRIC TESTS -------------------------------------------------------
+      mc.parametric,
       
       # NON-PARAMETRIC TESTS --------------------------------------------
-      tabPanel(
-        'Non-parametric',
-        titlePanel("Non-parametric tests for comparing means between samples"),
-        sidebarLayout(
-          sidebarPanel(
-            selectInput(
-              'mcnp.test.type',
-              'Test type',
-              choices = c(`One Sample` = "onesample",
-                          `Two Samples` = "twosamples")
-            ),
-            tags$hr(),
-            
-            uiOutput('select.variable.mcnp1'),
-            uiOutput('select.variable.mcnp2'),
-            tags$hr(),
-            
-            strong("T-TEST NON-PARAM OPTIONS"),
-            helpText("Choose option below when comparing one sample"),
-            numericInput('mu_np', 'Mean to compare with', 0),
-            
-            selectInput(
-              'par_wilcox',
-              'Paired',
-              choices = c(Paired = 'Paired',
-                          `Non-paired` = 'Non-paired')
-            ),
-            selectInput(
-              'alternative_wilcox',
-              'Alternative',
-              choices = c(
-                Less = "greater",
-                Greater = "less",
-                `Two Sided` = "two.sided"
-              )
-            ),
-            numericInput(
-              'alpha_wilcox',
-              'Significance level',
-              value = 0.05,
-              step = 0.01
-            ),
-            submitButton()
-          ),
-          mainPanel(verbatimTextOutput('means_nonparam_output'))
-        )
-      )
+      mc.nonparametric
     ),
     
-    # ANALYSIS OF VARIANCE PANEL -------------------------------------------------
+    # ANALYSIS OF VARIANCE PANEL -----------------------------------------------
     navbarMenu(
       'ANOVA',
       
       # PARAMETRIC TESTS ------------------------------------------------
-      tabPanel(
-        'Parametric',
-        titlePanel("Parametric tests for analysis of variance"),
-        sidebarLayout(
-          sidebarPanel(
-            strong("ANOVA test"),
-            
-            selectInput(
-              'aovp.test.type',
-              'Test type',
-              choices = c(`One Sample` = "onesample",
-                          `Two Samples` = "twosamples")
-            ),
-            tags$hr(),
-            
-            helpText("Categorical X1 variable"),
-            uiOutput('select.variable.aovp1'),
-            helpText("Categorical X2 variable"),
-            uiOutput('select.variable.aovp2'),
-            helpText("Dependent Y variable"),
-            uiOutput('select.variable.aovp3'),
-            
-            tags$hr(),
-            submitButton()
-          ),
-          mainPanel(verbatimTextOutput('anova_param_output'),
-                    width = 8)
-        )
-      ),
+      aov.parametric,
       
       # NON-PARAMETRIC TESTS --------------------------------------------
-      tabPanel(
-        'Non-parametric',
-        titlePanel("Non-parametric tests for analysis of variance"),
-        sidebarLayout(
-          sidebarPanel(
-            tags$hr(),
-            helpText("Categorical X1 variable"),
-            uiOutput('select.variable.aovnp1'),
-            helpText("Categorical Y variable"),
-            uiOutput('select.variable.aovnp2'),
-            
-            tags$hr(),
-            submitButton()
-          ),
-          mainPanel(verbatimTextOutput('anova_nonparam_output'))
-        )
-      )
+      aov.nonparametric
     ),
     
-    # LIMIT OF DETECTION PANEL ---------------------------------------------------
-    tabPanel(
-      'LOD',
-      titlePanel("Limit of detection calculation"),
-      tags$hr(),
-      mainPanel(
-        strong("Limit of Detection plot"),
-        plotlyOutput('lod_plots_output'),
-        tags$hr(),
-        strong("LOD Summary"),
-        verbatimTextOutput('lod_stats_output'),
-        width = 12
-      )
-    ),
+    # LIMIT OF DETECTION PANEL -------------------------------------------------
+    lod.ui,
     
-    # HIGH RESOLUTION MELT MENU --------------------------------------------------
+    # HIGH RESOLUTION MELT MENU ------------------------------------------------
     navbarMenu(
       'HRM',
       
       # MELTING CURVES VISUALISATION ------------------------------------
-      tabPanel(
-        'Fluorescence Visualisation',
-        titlePanel("qPCR Fluorescence plot visualisation"),
-        tags$hr(),
-        sidebarLayout(
-          sidebarPanel(
-            strong("Choose reference curve"),
-            uiOutput('ref.curve'),
-            tags$hr(),
-            submitButton()
-          ),
-          mainPanel(
-            plotlyOutput('qpcr_plot_output'),
-            plotlyOutput('qpcr_diff_output'),
-            width = 8
-          )
-        )
-      )
+      hrm.ui
     )
   )
 )
