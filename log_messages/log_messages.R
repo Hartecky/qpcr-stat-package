5575b2d3580537a5fc5856e78404bd356bd48326
Hartecky <hofman.bartlomiej@gmail.com>
2021-08-16 13:07:01 +0200

    added decision making brief info

--- log_messages/log_messages.R
+++ log_messages/log_messages.R
@@ -2,13 +2,13 @@ assumptions.messages <- function(option, variable) {
   if (option == 'normtest') {
     
     cat('Distribution normality testing:\n
-    H0: Distribution of variable', variable, 'is consistent with normal distribution
-    HA: Distribution of variable', variable, 'differs from normal distribution \n')
+        H0: Distribution of variable', variable, 'is consistent with normal distribution
+        HA: Distribution of variable', variable, 'differs from normal distribution \n')
     
   } else if (option == 'vartest') {
     cat('Variance Homogenity testing:\n
-    H0: The variance among each group is equal
-    HA: At least one group has a variance that is not equal to the rest\n')
+        H0: The variance among each group is equal
+        HA: At least one group has a variance that is not equal to the rest\n')
     
   } else if (option == 'outliers') {
     
@@ -23,5 +23,24 @@ assumptions.messages <- function(option, variable) {
         based on Z-score, by scalling data to new set with mean 0
         and standard deviation 1. After scalling, if the absolute 
         value X > 3, then this value is considered as outlier\n')
+  }
+}
+
+decision.making.messages <- function(option){
+  if (option == 'normtest') {
+    cat('Decision making based on p-value\n
+        
+        When p < 0.05 - Reject H0 and accept alternative, 
+        distribution of variable is from normal distribution.\n
+        Whenp > 0.05 - Fail to reject H0 - distribution of 
+        variable is not from normal distribution\n')
+  } else if (option == 'vartest') {
+    
+    cat('Decision making based on p-value\n
+        
+        When p < 0.05 - Reject H0 and accept alternative,
+        variances are equal among groups.\n
+        When p > 0.05 - Fail to reject H0 - variances among
+        groups are not equal\n')
   }
 }
