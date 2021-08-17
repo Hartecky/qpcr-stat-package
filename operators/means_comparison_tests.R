613443d405ddcbf4fe4fd9951d231f65f136b475
Hartecky <hofman.bartlomiej@gmail.com>
2021-08-17 16:42:36 +0200

    moved wilcox test to means_comparison_test.R

---
+++ operators/means_comparison_tests.R
@@ -0,0 +1,51 @@
+compare.means.param <- function(test.type, X, Y, mu, alternative, paired, confidence){
+  if (test.type == 'onesample') {
+    # one sample test
+    t.test(x = X, 
+           mu = mu,
+           conf.level = confidence)
+  } else if (test.type == 'twosamples' & paired == 'Paired') {
+    # two sample paired test 
+    t.test(x = X,
+           y = Y,
+           alternative = alternative,
+           paired = TRUE,
+           conf.level = confidence)
+  } else if (test.type == 'twosamples' & paired == 'Non-paired') {
+    # two sample non-paired test
+    t.test(x = X,
+           y = Y,
+           alternative = alternative,
+           paired = FALSE,
+           conf.level = confidence)
+  }
+}
+
+compare.means.nonparam <- function(test.type, X, Y, mu, alternative, paired, confidence){
+  
+  if (test.type == 'onesample') {
+    # one sample wilcox test
+    wilcox.test(X,
+                mu = mu,
+                alternative = alternative,
+                conf.level = confidence)
+  } else if (test.type == 'twosamples' & paired == 'Paired') {
+    # two samples wilcox paired test
+    wilcox.test(x = X,
+                y = Y,
+                alternative = alternative,
+                paired = TRUE,
+                conf.level = confidence)
+  } else if (test.type == 'twosamples' & paired == 'Non-paired') {
+    # two samples wilcox non-paired test
+    wilcox.test(x = X,
+                y = Y,
+                alternative = alternative,
+                paired = FALSE,
+                conf.level = confidence)
+  }
+}
+
+
+
+
