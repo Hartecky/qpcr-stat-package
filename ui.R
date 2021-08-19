1b6a91407fac77f8e56c4d0ae61b70b1427d8323
Hartecky <hofman.bartlomiej@gmail.com>
2021-08-19 14:08:34 +0200

    saved

--- ui.R
+++ ui.R
@@ -11,37 +11,6 @@ setwd(CURRENT.PATH)
 # NECESSARY LIBRIARIES ---------------------------------------------------------
 source('packages.R')
 
-# VALIDATION
-source('condition_validation.R')
-
-# USER INTERFACE COMPONENTS ----------------------------------------------------
-source('ui/aov_nonparametric_ui.R')
-source('ui/aov_parametric_ui.R')
-source('ui/assumptions_ui.R')
-source('ui/data_upload_ui.R')
-source('ui/data_vis_ui.R')
-source('ui/hrm_ui.R')
-source('ui/lod_ui.R')
-source('ui/mc_nonparametric_ui.R')
-source('ui/mc_parametric_ui.R')
-
-# LOGS FILES
-source('log_messages/log_messages.R')
-
-# OPERATORS SOURCE FILES -----------------------------------------------------
-source('operators/configure_lod_set.R')
-source('operators/preprocessing_lod_data.R')
-source('operators/melt_data.R')
-source('operators/diff_curve_calc.R')
-source('operators/assumptions.R')
-source('operators/anova_tests.R')
-source('operators/means_comparison_tests.R')
-
-# PLOTTING FUNCTIONS
-source('plots/base_plot.R')
-source('plots/fluorescence_plot.R')
-source('plots/generate_lod_plot.R')
-
 # UI FOR WHOLE APPLICATION -----------------------------------------------------
 ui <- fluidPage(
   theme = shinytheme("darkly"),
