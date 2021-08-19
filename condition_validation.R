07cb18db364fce4b06c4a57fe0904a2b5bf0ac49
Hartecky <hofman.bartlomiej@gmail.com>
2021-08-19 14:05:56 +0200

    condition_validation - added comments | server and ui - changed name of the title header | packages.R - Added loading source files through packages instead from 100x source() in ui

--- condition_validation.R
+++ condition_validation.R
@@ -1,3 +1,11 @@
+# Checks range of provided alpha value
+
+# Parameters: 
+# alpha: value provided by user
+
+# Returns TRUE if alpha is [0,1] and 
+# returns message when it is not
+
 check.alpha <- function(alpha){
   if (alpha > 1 | alpha < 0) {
     stop("Confidence level must be a single number between 0 and 1")
--- packages.R
+++ packages.R
@@ -1,5 +1,4 @@
 # Packages names
-
 packages <- c("shiny",
               "plotly",
               "splines",
@@ -12,6 +11,33 @@ packages <- c("shiny",
               "shinydashboard",
               "outliers")
 
+# Additional source file names
+additional.files <- c("ui/aov_nonparametric_ui.R",
+                      "ui/aov_parametric_ui.R",
+                      "ui/assumptions_ui.R",
+                      "ui/data_upload_ui.R",
+                      "ui/data_vis_ui.R",
+                      "ui/hrm_ui.R",
+                      "ui/lod_ui.R",
+                      "ui/mc_nonparametric_ui.R",
+                      "ui/mc_parametric_ui.R",
+                      
+                      "log_messages/log_messages.R",
+                      
+                      "operators/configure_lod_set.R",
+                      "operators/preprocessing_lod_data.R",
+                      "operators/melt_data.R",
+                      "operators/diff_curve_calc.R",
+                      "operators/assumptions.R",
+                      "operators/anova_tests.R",
+                      "operators/means_comparison_tests.R",
+                      
+                      "plots/base_plot.R",
+                      "plots/fluorescence_plot.R",
+                      "plots/generate_lod_plot.R",
+                      
+                      "condition_validation.R")
+
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
 if (any(installed_packages == FALSE)) {
@@ -20,3 +46,6 @@ if (any(installed_packages == FALSE)) {
 
 # Packages loading
 invisible(lapply(packages, library, character.only = TRUE))
+
+# Source files loading
+invisible(lapply(additional.files, source))
