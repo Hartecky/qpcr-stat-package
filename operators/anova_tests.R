6f64f7cdac650076462bdfdbbe2a0dce54ed320e
Hartecky <hofman.bartlomiej@gmail.com>
2021-08-23 13:14:37 +0200

    added comments for anova tests

--- operators/anova_tests.R
+++ operators/anova_tests.R
@@ -1,3 +1,15 @@
+# This script contains functions which are performing means comparison
+# between groups :
+# One factor ANOVA
+# Two factor ANOVA
+# TukeyHS PostHoc test
+# Non-parametric Kruskal-Wallis test
+
+# Fits one factor ANOVA, and returns fitted model
+# Parameters:
+# Y     - dependent factor
+# X     - independent factor
+# data  - dataframe
 analysis.of.variance <- function(Y, X, data) {
   
   fit <- aov(Y ~ X, data = data)
@@ -6,6 +18,12 @@ analysis.of.variance <- function(Y, X, data) {
               model = fit))
 }
 
+# Fits two factor ANOVA, and returns fitted model
+# Parameters:
+# Y     - dependent variable
+# X1    - first independent factor
+# X2    - second independent factor
+# data  - dataframe
 analysis.of.variance.two <- function(Y, X1, X2, data){
   
   fit <- aov(Y ~ X1 + X2, data = data)
@@ -14,6 +32,11 @@ analysis.of.variance.two <- function(Y, X1, X2, data){
               model = fit))
 }
 
+# Performs TukeyHSD post-hoc test if provided model indicates that
+# one of the provided variables is significant
+# Parameters:
+# model           - anova results model
+# fitted.model    - fitted anova model
 analysis.posthoc <- function(model, fitted.model) {
   if (fitted.model$`Pr(>F)`[1] < 0.05) {
     TukeyHSD(model)
@@ -22,11 +45,23 @@ analysis.posthoc <- function(model, fitted.model) {
   }
 }
 
+# Performs one factor non-parametric means comparison between groups
+# Parameters:
+# Y     - dependent variable
+# X     - independent factor
+# data  - dataframe
 kruskal.analysis <- function(Y, X, data){
   model <- kruskal.test(Y ~ X, data)
   print(model)
 }
 
+# Performs Duncan non-parametric post-hoc test if model indicates that
+# dependent variable is significant. Returns letter designations for groups
+# Parameters:
+# Y     - dependent variable
+# X     - independent variable
+# model - kruskal-wallis model
+# data  - dataframe
 kruskal.posthoc <- function(Y, X, model, data){
   if (model$p.value < 0.05){
     DT <- dunnTest(Y ~ X, data = data)
@@ -40,6 +75,14 @@ kruskal.posthoc <- function(Y, X, model, data){
   }
 }
 
+# Wrapped function which is testing variables properties and 
+# performing desired parametric tests via shiny app
+# Parameters:
+# option    - option provided from user by input
+# Y         - dependent variable
+# X1        - first independent factor
+# X2        - second independent factor
+# data      - dataframe
 anova.test.param <- function(option, Y, X1, X2, data){
   if (option == 'onesample'){
     stopifnot(is.factor(X1))
@@ -68,6 +111,14 @@ anova.test.param <- function(option, Y, X1, X2, data){
   }
 }
 
+# Wrapped function which is testing variables properties and 
+# performing desired non-parametric test via shiny app
+# Parameters:
+# option    - option provided from user by input
+# Y         - dependent variable
+# X1        - first independent factor
+# X2        - second independent factor
+# data      - dataframe
 anova.test.nonparam <- function(Y, X, data) {
   stopifnot(is.factor(X))
   stopifnot(is.vector(Y) | is.numeric(Y) | length(Y) < 2)
